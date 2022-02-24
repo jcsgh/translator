d176546f556e68d8c7c24fd8e92d5a1cdd81d356
jcsgh <business.joshuac@gmail.com>
2022-02-24 12:02:31 -0600

    working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import { useEffect, useState } from 'react';
+import React from 'react';
+const axios = require('axios').default;
 
 function App() {
+
+  const [options, setOptions] = useState([]);
+  const [to, setTo] = useState(''); 
+  const [from, setFrom] = useState(''); 
+  const [input, setInput] = useState(''); 
+  const [output, setOutput] = useState(''); 
+
+  useEffect(()=>{
+
+    axios.get("https://libretranslate.com/languages",
+     {headers:{'accept':'application/json'}}).then(res=>{
+       console.log(res.data);
+       setOptions(res.data);
+     })
+     .catch((err) => {
+       console.log(err)
+     })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header/>
+
+      <div>
+        From:
+        <select>
+          {options.map(opt => <option value={opt.code}>{opt.name}</option>)}
+        </select>
+        To:
+        <select>
+          {options.map(opt => <option value={opt.code}>{opt.name}</option>)}
+        </select>
+        <div>
+          <textarea col="50" rows="8"></textarea>
+        </div>
+        <div>
+          <textarea col="50" rows="8"></textarea>
+        </div>
+        <div>
+          <button >Translate</button>
+        </div>
+      </div>
+
+      <Footer/>
     </div>
-  );
+  )
 }
 
 export default App;
